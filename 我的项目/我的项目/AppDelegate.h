df8f44b135345b329ba5a816df6d796ec58917c0
刀哥 <@qq.com>
2016-07-13 20:33:32 +0800

    初始化项目

---
+++ 我的项目/我的项目/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  我的项目
+//
+//  Created by apple on 16/7/13.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
